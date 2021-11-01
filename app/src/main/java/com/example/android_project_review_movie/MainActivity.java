206d0a18c9c7407d03e96e2d211c89b8441650a5
Hyunjin Cho <jhj176610@gmail.com>
2021-10-31 23:59:55 -0500

    Home Design

--- app/src/main/java/com/example/android_project_review_movie/MainActivity.java
+++ app/src/main/java/com/example/android_project_review_movie/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android_project_review_movie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar =  findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+//
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the options menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
