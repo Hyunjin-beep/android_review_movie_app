911925a2eee6eae49e8ddae80867d656fd2ad5dc
Hyunjin Cho <jhj176610@gmail.com>
2021-11-27 23:21:14 -0600

    Add reply function

---
+++ app/src/main/java/com/example/android_project_review_movie/ReplyAdapter.java
@@ -0,0 +1,2 @@
+package com.example.android_project_review_movie;public class ReplyAdapter {
+}
---
+++ app/src/main/java/com/example/android_project_review_movie/ReplyComment.java
@@ -0,0 +1,14 @@
+package com.example.android_project_review_movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReplyComment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply_comment);
+    }
+}
